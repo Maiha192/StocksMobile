4151cd17a3643307b4d3152ca76f1165279c1f54
maiha <maiha.219@gmail.com>
2023-12-10 08:08:05 +1000

    Add Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,36 @@
-import React from "react";
-import { Text } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet, Image, SafeAreaView } from 'react-native';
 
 export default function HomeScreen() {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image
+        source={require('../assets/logo.png')}
+        style={styles.logo}
+      />
+      <Text style={styles.brandName}>STOCKS SMART</Text>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: 150, 
+    height: 150, 
+    resizeMode: 'contain',
+    marginBottom: 20, 
+  },
+  brandName: {
+    fontSize: 32, 
+    fontWeight: 'bold', 
+    color: 'black',
+    marginTop: 20, 
+  },
+});
