677cc0b32c56c5ee0d1bdd711bbe1095abdea104
maiha <maiha.219@gmail.com>
2023-12-16 14:53:29 +1000

    Add comments to App.js

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+// Import necessary dependencies
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./navigation/BottomTabNavigator";
 import { FavoritesListProvider } from "./contexts/FavoritesListProvider";
 
+// Main function for the mobile app
 export default function App() {
   return (
     <FavoritesListProvider>
