86fe98b007d3e6677860ac546714b0c35e1c44bf
maiha <maiha.219@gmail.com>
2023-12-16 15:05:25 +1000

    Add comments to Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, SafeAreaView } from 'react-native';
+// Import necessary dependencies
+import React from "react";
+import { Text, StyleSheet, Image, SafeAreaView } from "react-native";
 
+// Main function for Home Screen
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image
-        source={require('../assets/logo.png')}
-        style={styles.logo}
-      />
+      <Image source={require("../assets/logo.png")} style={styles.logo} />
       <Text style={styles.brandName}>STOCKS SMART</Text>
     </SafeAreaView>
   );
 }
 
+// Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
   },
   logo: {
-    width: 150, 
-    height: 150, 
-    resizeMode: 'contain',
-    marginBottom: 20, 
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+    marginBottom: 20,
   },
   brandName: {
-    fontSize: 32, 
-    fontWeight: 'bold', 
-    color: 'black',
-    marginTop: 20, 
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "black",
+    marginTop: 20,
   },
 });
